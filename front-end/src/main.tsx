c88b989574e1b267c02f6b68ab827cb255e5de97
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-05-21 22:55:24 +0800

    Remove architects daughter font.

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,7 +4,6 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import "@fontsource/architects-daughter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Fonts } from "./components/Fonts";
